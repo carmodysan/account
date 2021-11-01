5ccc9aefda8c8c51f65c5186a58bfe8fff6911c2
carmodysan <carmodysan@outlook.com>
2021-11-01 17:27:55 +0100

    Ajout de la fonction Login Ajout de la fonction Logout

---
+++ src/Controller/SecurityController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class SecurityController extends AbstractController
+{
+    /**
+     * @Route("/logout", name="logout", methods={"GET"})
+     */
+    public function logout(): Response
+    {
+        // controller can be blank: it will never be called!
+    }
+}
