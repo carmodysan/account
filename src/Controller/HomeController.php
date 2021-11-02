bbc0e8d25a42488db4f0294777368e7616c20e8d
carmodysan <carmodysan@outlookcom>
2021-11-02 16:19:33 +0100

    Ajout homepage pour utilisateur

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -5,17 +5,28 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 
 class HomeController extends AbstractController
 {
     /**
-     * @Route("/")
-     * @Route("/home", name="home")
+     * @Route("/", name="home")
      */
     public function index(): Response
     {
-        return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
-        ]);
+        return $this->render('home/index.html.twig', []);
+    }
+
+    /**
+     * Homepage for authentificated user
+     * 
+     * @IsGranted("ROLE_USER")
+     * @Route("/home", name="home_auth")
+     *
+     * @return Response
+     */
+    public function homeAuth(): Response
+    {
+        return $this->render('home/index_auth.html.twig', []);
     }
 }
