4e75fb761d7ae59eb9ddd0df2c42c7831683e941
carmodysan <carmodysan@outlookcom>
2021-11-02 17:01:44 +0100

    Modification homepage utilisateur : - Ajout d'une sidebar

--- assets/app.js
+++ assets/app.js
@@ -10,3 +10,25 @@ import './styles/app.scss';
 
 // start the Stimulus application
 // import './bootstrap';
+
+const $ = require('jquery');
+// this "modifies" the jquery module: adding behavior to it
+// the bootstrap module doesn't export/return anything
+require('bootstrap');
+
+// or you can include specific pieces
+// require('bootstrap/js/dist/tooltip');
+// require('bootstrap/js/dist/popover');
+
+$(document).ready(function() {
+    $('[data-toggle="popover"]').popover();
+});
+
+// Dropdown
+(function () {
+    'use strict'
+    var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+    tooltipTriggerList.forEach(function (tooltipTriggerEl) {
+      new bootstrap.Tooltip(tooltipTriggerEl)
+    })
+})()
