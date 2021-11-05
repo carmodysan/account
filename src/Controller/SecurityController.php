f4a70969d1cbaf43df5f907482a9a504c14b7785
carmodysan <carmodysan@outlook.com>
2021-11-05 17:16:58 +0100

    Ajout d'une référence pour le controleur Security

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 
 class SecurityController extends AbstractController
 {
