28808c77d82677f9e8d2de6851d380fa6d8c67a9
carmodysan <carmodysan@outlook.com>
2021-11-05 10:18:58 +0100

    Modification page d'accueil

--- assets/vue/components/Home.vue
+++ assets/vue/components/Home.vue
@@ -1,19 +1,30 @@
 <template>
-    <div>
-        <p>This is an example of a new components in VueJs</p>
-        <v-btn elevation="2" href="/login">Home Vuetify</v-btn>
-        <v-divider></v-divider>
-        <v-skeleton-loader
-      class="mx-auto"
-      max-width="300"
-      type="card"
-    ></v-skeleton-loader>
-    </div>
+	<v-app>
+		<v-container fill-height fluid>
+			<v-row align="center" justify="center" class="d-flex flex-column">
+				<v-row class="d-flex justify-center">
+					<div class="text-h3 mb-6 text-center">Comptes mensuels</div>
+				</v-row>
+				<v-row class="d-flex justify-center mb-6 mx-3">
+					<v-card elevation="5" outlined max-width="700">
+						<div class="pa-2 text-center">
+							<p class="font-weight-light font-italic">
+								"Faire ses comptes est indépendant de faire un budget. Autrement dit, vous pouvez simplement commencer par tracer vos revenus et vos dépenses sans
+								avoir de budget précis. Le plus important, dans tous les cas, c'est de commencer."
+							</p>
+						</div>
+					</v-card>
+				</v-row>
+				<v-row class="d-flex justify-center">
+					<v-btn depressed color="primary" href="/login">Se connecter</v-btn>
+				</v-row>
+			</v-row>
+		</v-container>
+	</v-app>
 </template>
 <script>
-    export default {
-        name: "home"
-    }
+export default {
+	name: "home",
+};
 </script>
-<style scoped>
-</style>
+<style scoped></style>
