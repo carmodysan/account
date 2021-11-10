4016af72f9afdbffcee88c97b2a50d49b97ebba5
carmodysan <carmodysan@outlookcom>
2021-11-10 19:36:46 +0100

    Modification des pages d'erreur Suppression de code obsolète

--- assets/vue/app.js
+++ assets/vue/app.js
@@ -5,6 +5,7 @@ import Home from './components/Home';
 import MenuDesktop from './components/MenuDesktop.vue';
 import MenuMobile from './components/MenuMobile';
 import Error404 from './components/errors/404';
+import ErrorWeb from './components/errors/Error';
 
 /**
 * Create a fresh Vue Application instance
@@ -16,6 +17,7 @@ new Vue({
         Home, 
         'menu-desktop': MenuDesktop, 
         'menu-mobile': MenuMobile,
-        'error-404': Error404
+        'error-404': Error404,
+        'error': ErrorWeb
     }
 });
--- assets/vue/components/errors/Error.vue
+++ assets/vue/components/errors/Error.vue
@@ -3,13 +3,13 @@
 		<v-container fill-height fluid>
 			<v-row align="center" justify="center" class="d-flex flex-column">
 				<v-row class="d-flex justify-center">
-					<div class="text-h1 mb-6 text-center">404</div>
+					<div class="text-h1 mb-6 text-center">Erreur</div>
 				</v-row>
 				<v-row class="d-flex justify-center mb-6 mx-3">
 					<v-card elevation="5" outlined max-width="700">
 						<div class="pa-2 text-center">
 							<p class="font-weight-light font-italic">
-								"La page n'a pas été trouvée, mais nous pouvons continuer à la chercher ;-)"
+								"Dommage, ça ne marche pas, mais bon on se retrouve très bientôt ;-)"
 							</p>
 						</div>
 					</v-card>
@@ -18,12 +18,12 @@
 					<v-btn depressed color="primary" href="/home">Revenir à la page d'accueil</v-btn>
 				</v-row>
 			</v-row>
-        </v-container>
+		</v-container>
 	</v-app>
 </template>
 <script>
 export default {
-	name: "404",
+	name: "error",
 };
 </script>
 <style scoped></style>
