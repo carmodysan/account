d3e509306c55d0c119233c4d2786a27c7dc2ff36
carmodysan <carmodysan@outlook.com>
2021-11-12 08:40:54 +0100

    Modification de l'URL API de base

--- assets/vue/main.js
+++ assets/vue/main.js
@@ -6,7 +6,7 @@ import vuetify from './plugins/vuetify';
 import axios from 'axios';
 
 Vue.config.productionTip = false
-axios.defaults.baseURL = 'http://127.0.0.1:8000/api'
+axios.defaults.baseURL = process.env.BASE_URL_API
 
 new Vue({
     router,
